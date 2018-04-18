8243e3842da78f2abad7613eca32a62159988497
qingkaigoh <qingkai-goh@live.com>
2018-04-18 11:52:01 -0700

    header

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
 import App from './components/app';
+import Header from "./components/header";
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
+            <Header/>
             <Switch>
                 <Route path="/" component={App}/>
             </Switch>
         </div>
     </BrowserRouter>
-    , document.querySelector('.container'));
+    , document.querySelector('.root'));
